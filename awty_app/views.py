ac6ddcecadb16a447d33c20706c7a05740875550
Carson-Spaniel <carsonspaniel@gmail.com>
2024-10-29 13:27:04 -0500

    testing login functionality

--- awty_app/views.py
+++ awty_app/views.py
@@ -2,12 +2,12 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 @login_required
-def home(request, path=''):
+def home(request):
     return render(request, 'index.html')
 
 @login_required
 def trips(request, trip_id=''):
     return render(request, 'index.html')
 
-def login(request, path=''):
+def login(request):
     return render(request, 'index.html')
